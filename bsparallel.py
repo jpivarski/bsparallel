f2aa4bfa41041a016b0141a66f542c9ea02d89f1
Jim Pivarski <jpivarski@gmail.com>
2024-08-15 18:45:58 -0500

    add a completion counter

--- bsparallel.py
+++ bsparallel.py
@@ -4,6 +4,7 @@ import traceback
 import multiprocessing
 
 arguments = multiprocessing.Queue()
+counter = multiprocessing.Value("i", 0)
 dead = multiprocessing.Event()
 
 
@@ -11,7 +12,7 @@ def timestamp():
     return time.strftime("%H:%M ")
 
 
-def work(identifier, task):
+def work(identifier, task, total):
     def modified_print(*args, **kwargs):
         args = (timestamp() + identifier + ":",) + args
         kwargs["flush"] = True
@@ -32,29 +33,44 @@ def work(identifier, task):
 
         try:
             task(*args)
+
         except Exception:
             stacktrace = traceback.format_exc().split("\n")
             ts = timestamp()
             builtins.print(
-                "\n".join(f"{ts}{identifier}: {line}" for line in stacktrace), flush=True
+                "\n".join(f"{ts}{identifier}: {line}" for line in stacktrace),
+                flush=True,
             )
             dead.set()
             builtins.print(ts + "END WITH ERROR", identifier, flush=True)
             return
+
+        else:
+            ts = timestamp()
+            with counter.get_lock():
+                counter.value += 1
+                counter_now = counter.value
+            builtins.print(
+                ts + f"FINISHED {100*counter_now/total:.0f}% ({counter_now}/{total})"
+            )
 
 
 workers = []
 
 
 def run(num_workers, task, inputs):
+    total = 0
     for x in inputs:
         if not isinstance(x, tuple):
             x = (x,)
         arguments.put(x)
+        total += 1
 
     for i in range(num_workers):
         arguments.put(None)
-        workers.append(multiprocessing.Process(target=work, args=(f"WORKER-{i}", task)))
+        workers.append(
+            multiprocessing.Process(target=work, args=(f"WORKER-{i}", task, total))
+        )
 
     for worker in workers:
         worker.start()
